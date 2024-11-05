234ff267b57fc21374d3df046984ecf5f2747926
Giulio Riondino <riondino.giulio@gmail.com>
2024-11-04 21:56:51 -0800

    Create add book modal

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const myLibrary = []
 const cardSection = document.querySelector('.main .spacing')
 const addBookButton = document.querySelector('.add-book')
+const form = document.querySelector('dialog form')
+const dialog = document.querySelector('dialog')
 
 function Book(title, author, read) {
     this.title = title
@@ -44,3 +46,25 @@ addBookToLibrary('Discipline is Destiny', 'Ryan Holiday', true)
 addBookToLibrary('The Obstacle is the Way', 'Ryan Holiday', false)
 addBookToLibrary('Meditations', 'Marcus Aurelius', false)
 displayBooks()
+
+addBookButton.addEventListener('click', () => {
+    dialog.showModal()
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(form)
+    const title = formData.get('book-title')
+    const author = formData.get('book-author')
+    const read = formData.get('read')
+
+    addBookToLibrary(title, author, read)
+    dialog.close()
+})
+
+dialog.addEventListener('click', (e) => {
+    if (e.target === dialog) {
+        dialog.close()
+    }
+})
